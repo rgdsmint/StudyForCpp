1719faa2b65f4006f59532e62f777e736418ea28
rgdsmint <rgdsmint@gmail.com>
2019-05-25 12:55:50 +0800

    add

---
+++ Chapter1/Test1/NameAndAddress/NameAndAddress.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "I'm Xiaomin from the earth " << endl;
+    return 0;
+}
---
+++ Chapter2/Part3Array/CteateArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cards[2];
+    cards[0] = 1;
+    cards[1] = 2;
+
+    int nums[3] = {3, 4, 5};
+    
+    int nums2[] = {2222, 3333};
+    
+    double dous[2] {1.1, 2.2};
+
+}
