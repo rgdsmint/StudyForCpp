970a013f649d794c9da95bdfcda309db83816fcb
rgdsmint <rgdsmint@gmail.com>
2019-06-01 22:01:18 +0800

    输入

--- Chapter2/Part4Input/Input.cpp
+++ Chapter2/Part4Input/Input.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
+    //cin    
+    //cin以换行制表符空格为终止
+    //cin.getline()读取整行 不保留换行符
+    //cin.get()读取整行 保留换行符
+    string name;
+    cout << "请输入名字:";
+    cin >> name;
+    cout << "很高兴认识你, "<< name << endl;
     return 0;	
 }
