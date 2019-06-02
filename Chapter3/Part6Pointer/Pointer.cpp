7404c54d84b95196aedefdce3627741162032d13
rgdsmint <rgdsmint@gmail.com>
2019-06-02 08:18:46 +0800

    Pointer

--- Chapter3/Part6Pointer/Pointer.cpp
+++ Chapter3/Part6Pointer/Pointer.cpp
@@ -1,3 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
 int main() {
+    string name = "xiaoming";
+    int age = 10;
+    cout << "age is " << age << endl;
+    int* address = &age;
+    cout << "and age's address is " << address << endl;
     return 0;
 }
