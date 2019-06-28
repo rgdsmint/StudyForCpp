76eecd4590864ce133f7c4d26cd93c358cba655a
rgdsmint <rgdsmint@gmail.com>
2019-06-28 14:36:01 +0800

    add cpp file

---
+++ Chapter1-Start/Section1-FirstCppProgram/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main() {    // 主函数 入口函数 名字唯一且必须为此
+    cout << "Hello world!" << endl;
+}
---
+++ Chapter1-Start/Section2-MeetIO/IO.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+    包含输入流(istream), 输出流(ostream)
+    istream: cin
+    ostream: cout(标准输出), cerr(输出错误信息), clog(输出程序运行时的一般信息)
+ */
+using namespace std; 
+/*
+    引用名为"std"的命名空间, 包含cin, cout等
+    不引用则为std::cin  std::cout依此类推
+*/
+int main() {
+    cout << "请输入两个数: " << endl;
+    int v1, v2;
+    cin >> v1 >> v2;
+    cout << v1 << "与" << v2 << "的和为" << v1 + v2 << endl; 
+}
