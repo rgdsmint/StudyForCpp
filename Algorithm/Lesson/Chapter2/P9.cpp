723fbb79875efa2eb9f735a11f6431987eedf684
rgdsmint <rgdsmint@gmail.com>
2019-09-07 13:09:26 +0800

    数据统计

---
+++ Algorithm/Lesson/Chapter2/P9.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+int main() {
+	int x, n = 0, min, max, s = 0;
+	while (scanf("%d", &x) == 1) {
+		s += x;
+		n++;
+		if (x < min) {
+			min = x;
+		} 
+		if (x > max) {
+			max = x;
+		}
+	}
+	printf("%d %d %.3f\n", min, max, (double)s/n);
+	return 0;
+}
