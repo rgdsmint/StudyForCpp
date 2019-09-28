031373a8975d0d368d800c5089175ba8ced86e10
rgdsmint <rgdsmint@gmail.com>
2019-09-28 23:57:02 +0800

    数据统计

---
+++ Algorithm/Lesson/Chapter2/P10.cpp
@@ -0,0 +1,23 @@
+#define LOCAL
+#include <cstdio>
+#define INF 1000000000
+
+int main() {
+#ifdef LOCAL
+	freopen("data.in", "r", stdin);
+	freopen("data.out", "w", stdout);
+#endif
+	int x, n = 0, min = INF, max = -INF, s = 0;
+	while(scanf("%d", &x) == 1) {
+		s += x;
+		if(x < min) {
+			min = x;
+		} 
+		if (x > max) {
+			max = x;
+		}
+		n++;
+	}
+	printf("%d %d %.3f\n", min, max, (double)s / n);
+	return 0;
+}
