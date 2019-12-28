95c5951adc3e85b308f31e4de672da2a92373cb7
rgdsmint <rgdsmint@gmail.com>
2019-12-29 01:34:42 +0800

    d

--- Algorithm/Practice/Chapter2/subsequence.cpp
+++ Algorithm/Practice/Chapter2/subsequence.cpp
@@ -6,6 +6,7 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     int n, m, count = 1;
+    
     while (cin >> n >> m && (n != 0 || m != 0)) {
         double sum = 0.0;
         for (int i = n; i <= m; i++) {
