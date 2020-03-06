9a8dd8516743d907ce0977106b4f1a55eb77b061
rgdsmint <rgdsmint@gmail.com>
2020-03-06 17:55:48 +0800

    add

---
+++ Language/Book1/Chapter1/Section1/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string userName;
+    cout << "Please enter your first name: ";
+    cin >> userName;
+    cout << "\n" << "Hello, " << userName << "...and, goodbye!" << endl;
+}
